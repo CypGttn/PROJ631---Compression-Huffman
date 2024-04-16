daea59030556634dc1646fc19b02ae24fe32c9d7
CypGttn <154144790+CypGttn@users.noreply.github.com>
2024-04-16 10:30:00 +0200

    frequence de chaque caractère
    + Doublons enlevés

--- Huffman/src/App.java
+++ Huffman/src/App.java
@@ -1,8 +1,10 @@
-
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
         String fic = LectureFichier.LireFichier("C:\\Users\\Guitton Cyprien\\Downloads\\PROJ631 -Algo\\PROJ631---Compression-Huffman\\textesimple.txt");
-        System.out.println(Frequence.FrequenceCaractere(fic));
+        ArrayList<String> avecDoublons = Frequence.FrequenceCaractere(fic);
+        ArrayList<String> sansDoublons = Frequence.Doublons(avecDoublons);
+        System.out.println(Frequence.Count(avecDoublons, sansDoublons));
     }
 }
--- Huffman/src/Frequence.java
+++ Huffman/src/Frequence.java
@@ -18,4 +18,33 @@ public class Frequence {
 		}
         return decompoMot; 
     }
+
+    public static ArrayList<String> Doublons(ArrayList<String> decompoMot) {
+        ArrayList<String> enleverDoublons = new ArrayList<String>();
+
+        for (int i =0; i< decompoMot.size(); i++ ) {
+            if (!enleverDoublons.contains(decompoMot.get(i))) {
+                enleverDoublons.add(decompoMot.get(i));
+            }
+        }
+        return enleverDoublons;
+    }
+
+    public static ArrayList<Integer> Count(ArrayList<String> decompoMot, ArrayList<String> enleverDoublons){
+        ArrayList<Integer> frequenceLettre = new ArrayList<Integer>();
+        for (int i = 0; i < enleverDoublons.size(); i++) {
+            String element = enleverDoublons.get(i);
+            int count = 0; 
+            for (int j = 0; j < decompoMot.size(); j++) {
+                String lettre = decompoMot.get(j);
+                if (lettre.equals(element)){
+                    count++; 
+                }
+            }
+            frequenceLettre.add(count);
+
+         
+        }   
+        return frequenceLettre;
+    }
 }
