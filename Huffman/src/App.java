b1bf7b86826ca3de274f735333b18dfd1816ea17
CypGttn <154144790+CypGttn@users.noreply.github.com>
2024-04-19 13:51:11 +0200

    Feuille + début construction

--- Huffman/src/App.java
+++ Huffman/src/App.java
@@ -3,7 +3,7 @@ import java.util.LinkedHashMap;
 
 public class App {
     public static void main(String[] args) {
-        String fic = LectureFichier.LireFichier("C:\\Users\\Guitton Cyprien\\Downloads\\PROJ631 -Algo\\PROJ631---Compression-Huffman\\alice.txt");
+        String fic = LectureFichier.LireFichier("C:\\Users\\Guitton Cyprien\\Downloads\\PROJ631 -Algo\\PROJ631---Compression-Huffman\\extraitalice.txt");
         ArrayList<String> avecDoublons = Frequence.FrequenceCaractere(fic);
         ArrayList<String> sansDoublons = Frequence.Doublons(avecDoublons);
         ArrayList<Integer> frequenceLettre = Frequence.Count(avecDoublons, sansDoublons) ;
--- Huffman/src/Arbre.java
+++ Huffman/src/Arbre.java
@@ -1,3 +1,22 @@
 public class Arbre {
+    public String caractere;
+    public int valeur;
+    public Arbre enfantDroit;
+    public Arbre enfantGauche;
 
+    public Arbre(String caractere, int valeur, Arbre enfantDroit, Arbre enfantGauche) {
+        this.caractere = caractere;
+        this.valeur = valeur;
+        this.enfantDroit = enfantDroit;
+        this.enfantGauche = enfantGauche;
+    }
+
+    public Arbre(String caractere, int valeur) {
+        this.caractere = caractere;
+        this.valeur = valeur;
+    }
+
+    
+
+    
 }
