65a880d103a9f4310e8d1666060fcd978e9fb330
CypGttn <154144790+CypGttn@users.noreply.github.com>
2024-04-29 14:24:18 +0200

    test alice

--- Huffman/src/App.java
+++ Huffman/src/App.java
@@ -6,7 +6,7 @@ import java.util.LinkedHashMap;
 public class App {
     public static void main(String[] args) {
         //Chemin du fichier
-        String nomFichier = "extraitalice"; 
+        String nomFichier = "alice"; 
         String filePath = nomFichier + ".txt";
         //Lecture du fichier à partir d'un chemin donné
         String fic = LectureFichier.LireFichier(filePath);
